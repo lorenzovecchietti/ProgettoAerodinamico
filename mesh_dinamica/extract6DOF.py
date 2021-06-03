e6c1ba4e11358235b407a314b493f4f61b3ccd1f
fe0202 <federico.evangelista@outlook.it>
2021-06-03 20:56:46 +0200

    caricato case dinamico

---
+++ mesh_dinamica/extract6DOF.py
@@ -0,0 +1,21 @@
+sourcefile = 'log.interFoam'
+
+
+fpw = open('6DOF.dat', 'w')
+line_new = '{:<12}  {:>12}  {:>12}'.format('# Time', 'CM z', 'CM Uz')
+fpw.write(line_new + '\n')
+
+fp = open(sourcefile, 'r')
+for line in fp:
+	if line.startswith('Time = '):
+		time = line.rstrip().lstrip('Time = ')
+	if line.lstrip().startswith('Centre of mass'):
+		CMz = line.split(')')[0].split(' ')[-1]
+	if line.lstrip().startswith('Linear velocity'):
+		Uz = line.split(')')[0].split(' ')[-1]
+		line_new = '{:<12}  {:>12}  {:>12}'.format(time, CMz, Uz)
+		fpw.write(line_new + '\n')
+		
+
+fp.close()
+fpw.close()
